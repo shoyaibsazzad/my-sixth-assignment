27cfec0788385419acdb7844f7e966c426103108
shoyaibsazzad <shoyaibsazzad@gmail.com>
2025-09-08 18:13:35 +0600

    added API and DOM element in script.js

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 
+// ******API******
 
+const API = {
+  categories: "https://openapi.programming-hero.com/api/categories",
+  category: (id) => `https://openapi.programming-hero.com/api/category/${id}`,
+  plant: (id) => `https://openapi.programming-hero.com/api/plant/${id}`,
+};
+
+// ******DOM Elements*******
+
+const categoriesEl = document.getElementById("categories");
+const treesEl = document.getElementById("trees");
+const cartEl = document.getElementById("cart");
+const totalEl = document.getElementById("total");
+const modal = document.getElementById("modal");
+const modalContent = document.getElementById("modalContent");
+const closeModalBtn = document.getElementById("closeModal");
+
+
+let activeCategoryId = null;
+let cart = [];
+let total = 0;
